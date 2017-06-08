10a4e0ad097ff7bf455750735e994e48c1bb4553
MorisatoK <accounts@badlands-of-my-mind.de>
2017-06-08 23:15:31 +0200

    Projektdateien hinzufügen.

---
+++ iROBSceneChanger/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace iROBSceneChanger
+{
+    partial class iROBSceneChangerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.iRGroupBox = new System.Windows.Forms.GroupBox();
+            this.trackLabel = new System.Windows.Forms.Label();
+            this.driverLabel = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.OBSGroupBox = new System.Windows.Forms.GroupBox();
+            this.inGarageSceneTextBox = new System.Windows.Forms.TextBox();
+            this.inGarageLabel = new System.Windows.Forms.Label();
+            this.inCarSceneTextBox = new System.Windows.Forms.TextBox();
+            this.inCarLabel = new System.Windows.Forms.Label();
+            this.currentSceneLabel = new System.Windows.Forms.Label();
+            this.txtServerPasswordLabel = new System.Windows.Forms.Label();
+            this.txtServerIPLabel = new System.Windows.Forms.Label();
+            this.txtServerPassword = new System.Windows.Forms.TextBox();
+            this.txtServerIP = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.iRGroupBox.SuspendLayout();
+            this.OBSGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // iRGroupBox
+            // 
+            this.iRGroupBox.Controls.Add(this.trackLabel);
+            this.iRGroupBox.Controls.Add(this.driverLabel);
+            this.iRGroupBox.Controls.Add(this.statusLabel);
+            this.iRGroupBox.Location = new System.Drawing.Point(10, 10);
+            this.iRGroupBox.Name = "iRGroupBox";
+            this.iRGroupBox.Size = new System.Drawing.Size(320, 110);
+            this.iRGroupBox.TabIndex = 17;
+            this.iRGroupBox.TabStop = false;
+            this.iRGroupBox.Text = "iRacing";
+            // 
+            // trackLabel
+            // 
+            this.trackLabel.AutoSize = true;
+            this.trackLabel.Location = new System.Drawing.Point(10, 78);
+            this.trackLabel.Name = "trackLabel";
+            this.trackLabel.Size = new System.Drawing.Size(63, 13);
+            this.trackLabel.TabIndex = 9;
+            this.trackLabel.Text = "IsOnTrack: ";
+            // 
+            // driverLabel
+            // 
+            this.driverLabel.AutoSize = true;
+            this.driverLabel.Location = new System.Drawing.Point(10, 49);
+            this.driverLabel.Name = "driverLabel";
+            this.driverLabel.Size = new System.Drawing.Size(41, 13);
+            this.driverLabel.TabIndex = 8;
+            this.driverLabel.Text = "Driver: ";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(10, 20);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(43, 13);
+            this.statusLabel.TabIndex = 7;
+            this.statusLabel.Text = "Status: ";
+            // 
+            // OBSGroupBox
+            // 
+            this.OBSGroupBox.Controls.Add(this.inGarageSceneTextBox);
+            this.OBSGroupBox.Controls.Add(this.inGarageLabel);
+            this.OBSGroupBox.Controls.Add(this.inCarSceneTextBox);
+            this.OBSGroupBox.Controls.Add(this.inCarLabel);
+            this.OBSGroupBox.Controls.Add(this.currentSceneLabel);
+            this.OBSGroupBox.Controls.Add(this.txtServerPasswordLabel);
+            this.OBSGroupBox.Controls.Add(this.txtServerIPLabel);
+            this.OBSGroupBox.Controls.Add(this.txtServerPassword);
+            this.OBSGroupBox.Controls.Add(this.txtServerIP);
+            this.OBSGroupBox.Controls.Add(this.btnConnect);
+            this.OBSGroupBox.Location = new System.Drawing.Point(340, 10);
+            this.OBSGroupBox.Name = "OBSGroupBox";
+            this.OBSGroupBox.Size = new System.Drawing.Size(366, 110);
+            this.OBSGroupBox.TabIndex = 18;
+            this.OBSGroupBox.TabStop = false;
+            this.OBSGroupBox.Text = "OBS Websocket";
+            // 
+            // inGarageSceneTextBox
+            // 
+            this.inGarageSceneTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.inGarageSceneTextBox.Location = new System.Drawing.Point(242, 46);
+            this.inGarageSceneTextBox.Name = "inGarageSceneTextBox";
+            this.inGarageSceneTextBox.Size = new System.Drawing.Size(110, 20);
+            this.inGarageSceneTextBox.TabIndex = 23;
+            this.inGarageSceneTextBox.Text = "InGame";
+            // 
+            // inGarageLabel
+            // 
+            this.inGarageLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.inGarageLabel.AutoSize = true;
+            this.inGarageLabel.Location = new System.Drawing.Point(188, 49);
+            this.inGarageLabel.Name = "inGarageLabel";
+            this.inGarageLabel.Size = new System.Drawing.Size(45, 13);
+            this.inGarageLabel.TabIndex = 22;
+            this.inGarageLabel.Text = "Garage:";
+            // 
+            // inCarSceneTextBox
+            // 
+            this.inCarSceneTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.inCarSceneTextBox.Location = new System.Drawing.Point(64, 46);
+            this.inCarSceneTextBox.Name = "inCarSceneTextBox";
+            this.inCarSceneTextBox.Size = new System.Drawing.Size(110, 20);
+            this.inCarSceneTextBox.TabIndex = 21;
+            this.inCarSceneTextBox.Text = "Driving";
+            // 
+            // inCarLabel
+            // 
+            this.inCarLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.inCarLabel.AutoSize = true;
+            this.inCarLabel.Location = new System.Drawing.Point(10, 49);
+            this.inCarLabel.Name = "inCarLabel";
+            this.inCarLabel.Size = new System.Drawing.Size(38, 13);
+            this.inCarLabel.TabIndex = 20;
+            this.inCarLabel.Text = "In Car:";
+            // 
+            // currentSceneLabel
+            // 
+            this.currentSceneLabel.AutoSize = true;
+            this.currentSceneLabel.Location = new System.Drawing.Point(133, 80);
+            this.currentSceneLabel.Name = "currentSceneLabel";
+            this.currentSceneLabel.Size = new System.Drawing.Size(0, 13);
+            this.currentSceneLabel.TabIndex = 19;
+            // 
+            // txtServerPasswordLabel
+            // 
+            this.txtServerPasswordLabel.AutoSize = true;
+            this.txtServerPasswordLabel.Location = new System.Drawing.Point(185, 20);
+            this.txtServerPasswordLabel.Name = "txtServerPasswordLabel";
+            this.txtServerPasswordLabel.Size = new System.Drawing.Size(56, 13);
+            this.txtServerPasswordLabel.TabIndex = 18;
+            this.txtServerPasswordLabel.Text = "Password:";
+            // 
+            // txtServerIPLabel
+            // 
+            this.txtServerIPLabel.AutoSize = true;
+            this.txtServerIPLabel.Location = new System.Drawing.Point(10, 20);
+            this.txtServerIPLabel.Name = "txtServerIPLabel";
+            this.txtServerIPLabel.Size = new System.Drawing.Size(53, 13);
+            this.txtServerIPLabel.TabIndex = 17;
+            this.txtServerIPLabel.Text = "IP:PORT:";
+            // 
+            // txtServerPassword
+            // 
+            this.txtServerPassword.Location = new System.Drawing.Point(242, 16);
+            this.txtServerPassword.Name = "txtServerPassword";
+            this.txtServerPassword.Size = new System.Drawing.Size(110, 20);
+            this.txtServerPassword.TabIndex = 16;
+            this.txtServerPassword.Text = "bl4f4s3l";
+            this.txtServerPassword.UseSystemPasswordChar = true;
+            // 
+            // txtServerIP
+            // 
+            this.txtServerIP.Location = new System.Drawing.Point(64, 16);
+            this.txtServerIP.Name = "txtServerIP";
+            this.txtServerIP.Size = new System.Drawing.Size(110, 20);
+            this.txtServerIP.TabIndex = 15;
+            this.txtServerIP.Text = "ws://127.0.0.1:4444";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConnect.Location = new System.Drawing.Point(13, 75);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(100, 23);
+            this.btnConnect.TabIndex = 14;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // iROBSceneChangerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 131);
+            this.Controls.Add(this.OBSGroupBox);
+            this.Controls.Add(this.iRGroupBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "iROBSceneChangerForm";
+            this.Text = "iROBSceneChanger";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.iRGroupBox.ResumeLayout(false);
+            this.iRGroupBox.PerformLayout();
+            this.OBSGroupBox.ResumeLayout(false);
+            this.OBSGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox iRGroupBox;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Label trackLabel;
+        private System.Windows.Forms.Label driverLabel;
+        private System.Windows.Forms.GroupBox OBSGroupBox;
+        private System.Windows.Forms.TextBox inGarageSceneTextBox;
+        private System.Windows.Forms.Label inGarageLabel;
+        private System.Windows.Forms.TextBox inCarSceneTextBox;
+        private System.Windows.Forms.Label inCarLabel;
+        private System.Windows.Forms.Label currentSceneLabel;
+        private System.Windows.Forms.Label txtServerPasswordLabel;
+        private System.Windows.Forms.Label txtServerIPLabel;
+        private System.Windows.Forms.TextBox txtServerPassword;
+        private System.Windows.Forms.TextBox txtServerIP;
+        private System.Windows.Forms.Button btnConnect;
+    }
+}
+
---
+++ iROBSceneChanger/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using iRacingSdkWrapper;
+using OBSWebsocketDotNet;
+
+namespace iROBSceneChanger
+{
+    public partial class iROBSceneChangerForm : Form
+    {
+        private SdkWrapper wrapper;
+        private OBSWebsocket obs;
+
+        private Driver me;
+        private List<Driver> drivers;
+        private bool isUpdatingDrivers;
+        private bool oldIsOnTrack;
+        private bool obsWsConnected;
+
+        public iROBSceneChangerForm()
+        {
+            InitializeComponent();
+
+            // Create a new instance of the SdkWrapper object
+            wrapper = new SdkWrapper();
+
+            // Set some properties
+            wrapper.EventRaiseType = SdkWrapper.EventRaiseTypes.CurrentThread;
+            wrapper.TelemetryUpdateFrequency = 1;
+
+            // Listen for various iRacing events
+            wrapper.Connected += wrapper_Connected;
+            wrapper.Disconnected += wrapper_Disconnected;
+            wrapper.SessionInfoUpdated += wrapper_SessionInfoUpdated;
+            wrapper.TelemetryUpdated += wrapper_TelemetryUpdated;
+
+            // Bind a list of drivers to the grid
+            drivers = new List<Driver>();
+            oldIsOnTrack = false;
+
+            // Create a new instance of the OBSWebsocket object
+            obs = new OBSWebsocket();
+            obsWsConnected = false;
+
+            // Listen for various OBS events
+            obs.OnSceneChange += onSceneChange;
+
+            wrapper.Start();
+            StatusChanged();
+        }
+
+        #region Connecting, disconnecting, etc
+
+        private void StatusChanged()
+        {
+            if (wrapper.IsConnected)
+            {
+                if (wrapper.IsRunning)
+                {
+                    statusLabel.Text = "Status: connected!";
+                }
+                else
+                {
+                    statusLabel.Text = "Status: disconnected.";
+                }
+            }
+            else
+            {
+                if (wrapper.IsRunning)
+                {
+                    statusLabel.Text = "Status: disconnected, waiting for sim...";
+                }
+                else
+                {
+                    statusLabel.Text = "Status: disconnected";
+                }
+            }
+            driverLabel.Text = "Driver: ";
+            trackLabel.Text = "IsOnTrack: ";
+        }
+
+        private void wrapper_Connected(object sender, EventArgs e)
+        {
+            StatusChanged();
+        }
+
+        private void wrapper_Disconnected(object sender, EventArgs e)
+        {
+            StatusChanged();
+        }
+
+        #endregion
+
+        #region Events
+
+        private void wrapper_SessionInfoUpdated(object sender, SdkWrapper.SessionInfoUpdatedEventArgs e)
+        {
+            // Indicate that we are updating the drivers list
+            isUpdatingDrivers = true;
+
+            // Parse the Drivers section of the session info into a list of drivers
+            ParseDrivers(e.SessionInfo);
+
+            // Indicate we are finished updating drivers
+            isUpdatingDrivers = false;
+        }
+
+        private void wrapper_TelemetryUpdated(object sender, SdkWrapper.TelemetryUpdatedEventArgs e)
+        {
+            // Besides the driver details found in the session info, there's also things in the telemetry
+            // that are properties of a driver, such as their lap, lap distance, track surface, distance relative
+            // to yourself and more.
+            // We update the existing list of drivers with the telemetry values here.
+
+            // If we are currently renewing the drivers list it makes little sense to update the existing drivers
+            // because they will change anyway
+            if (isUpdatingDrivers) return;
+
+            UpdateDriversTelemetry(e.TelemetryInfo);
+            UpdateDriverLabels();
+            ChangeScene();
+        }
+
+        #endregion
+
+        #region Drivers
+
+        // Parse the YAML DriverInfo section that contains information such as driver id, name, license, car number, etc.
+        private void ParseDrivers(SessionInfo sessionInfo)
+        {
+            int id = 0;
+            Driver driver;
+
+            var newDrivers = new List<Driver>();
+
+            // Loop through drivers until none are found anymore
+            do
+            {
+                driver = null;
+
+                // Construct a yaml query that finds each driver and his info in the Session Info yaml string
+                // This query can be re-used for every property for one driver (with the specified id)
+                YamlQuery query = sessionInfo["DriverInfo"]["Drivers"]["CarIdx", id];
+
+                // Try to get the UserName of the driver (because its the first value given)
+                // If the UserName value is not found (name == null) then we found all drivers and we can stop
+                string name = query["UserName"].GetValue();
+
+                if (name != null)
+                {
+                    // Find this driver in the list
+                    // This strange " => " syntax is called a lambda expression and is short for a loop through all drivers
+                    // Read as: select the first driver 'd', if any, whose Name is equal to name.
+                    driver = drivers.FirstOrDefault(d => d.Name == name);
+
+                    if (driver == null)
+                    {
+                        // Or create a new Driver if we didn't find him before
+                        driver = new Driver();
+                        driver.Id = id;
+                        driver.Name = name;
+                        driver.CustomerId = int.Parse(query["UserID"].GetValue("0")); // default value 0
+                    }
+                    newDrivers.Add(driver);
+
+                    id++;
+                }
+            } while (driver != null);
+
+            // Replace old list of drivers with new list of drivers and update the grid
+            drivers.Clear();
+            drivers.AddRange(newDrivers);
+        }
+
+        private void UpdateDriversTelemetry(TelemetryInfo info)
+        {
+            // Get your own driver entry
+            // This strange " => " syntax is called a lambda expression and is short for a loop through all drivers
+            me = drivers.FirstOrDefault(d => d.Id == wrapper.DriverId);
+
+            if (me == null)
+                return;
+
+            me.IsOnTrack = info.IsOnTrack.Value;
+        }
+
+        private void UpdateDriverLabels()
+        {
+            if (me == null)
+                return;
+
+            driverLabel.Text = "Driver: " + me.Name;
+            trackLabel.Text = "IsOnTrack: " + me.IsOnTrack.ToString();
+        }
+
+        #endregion
+
+        #region OBS stuff
+        private void ChangeScene()
+        {
+            if (!obsWsConnected)
+                return;
+
+            if (string.IsNullOrEmpty(inCarSceneTextBox.Text) || string.IsNullOrEmpty(inGarageSceneTextBox.Text))
+                return;
+
+            if (me == null || me.IsOnTrack == oldIsOnTrack)
+                return;
+
+            if (me.IsOnTrack)
+                obs.SetCurrentScene(inCarSceneTextBox.Text);
+            else
+                obs.SetCurrentScene(inGarageSceneTextBox.Text);
+
+            oldIsOnTrack = me.IsOnTrack;
+        }
+
+        private void onSceneChange(OBSWebsocket sender, string newSceneName)
+        {
+            BeginInvoke((MethodInvoker)delegate
+            {
+                currentSceneLabel.Text = "Current Scene: " + newSceneName;
+            });
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(inCarSceneTextBox.Text) || string.IsNullOrEmpty(inGarageSceneTextBox.Text))
+            {
+                MessageBox.Show("You must provide OBS scenes.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            try
+            {
+                obs.Connect(txtServerIP.Text, txtServerPassword.Text);
+            }
+            catch (AuthFailureException)
+            {
+                MessageBox.Show("Authentication failed.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            catch (ErrorResponseException ex)
+            {
+                MessageBox.Show("Connect failed : " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            var scenes = obs.ListScenes();
+            var scenesFound = false;
+
+            foreach (var scene in scenes)
+            {
+                if (scene.Name == inCarSceneTextBox.Text || scene.Name == inGarageSceneTextBox.Text)
+                {
+                    scenesFound = true;
+                }
+            }
+
+            if (!scenesFound)
+            {
+                obs.Disconnect();
+                MessageBox.Show("Provided OBS scenes not found in scene collection.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            btnConnect.Enabled = false;
+            txtServerIP.Enabled = false;
+            txtServerPassword.Enabled = false;
+            obsWsConnected = true;
+
+            btnConnect.Text = "Connected";
+            currentSceneLabel.Text = "Current Scene: " + obs.GetCurrentScene().Name;
+        }
+        #endregion
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            wrapper.Stop();
+            obs.Disconnect();
+        }
+
+    }
+}
